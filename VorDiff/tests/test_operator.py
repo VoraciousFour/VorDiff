eb2b44496f397ebeba5791d632e6d335d6798877
狄伊烜 <yixuan_di@g.harvard.edu>
2019-11-15 16:09:18 -0500

    added test functions

--- VorDiff/tests/test_operator.py
+++ VorDiff/tests/test_operator.py
@@ -15,3 +15,57 @@ def test_sin():
     #Constant
     assert op.sin(c) == np.sin(c)
 
+def test_sinh():
+    # Scalar
+    f = op.sinh(x)
+    assert f._val == np.sinh(x._val)
+    assert f._der == np.cosh(x._val)*x_der
+
+    #Constant
+    assert op.sinh(c) == np.sinh(c)
+
+def test_cosh():
+    # Scalar
+    f = op.cosh(x)
+    assert f._val == np.cosh(x._val)
+    assert f._der == np.sinh(x._val)*x_der
+
+    #Constant
+    assert op.cosh(c) == np.cosh(c)
+
+def test_tanh():
+    # Scalar
+    f = op.tanh(x)
+    assert f._val == np.tanh(x._val)
+    assert f._der == (1-np.tanh(x._val)**2)*x_der
+
+    #Constant
+    assert op.tanh(c) == np.tanh(c)
+
+def test_arcsinh():
+    # Scalar
+    f = op.arcsinh()
+    assert f._val == np.arcsinh(x._val)
+    assert f._der == (-np.arcsinh(x._val))*np.arctanh(x._val)*x_der
+
+    #Constant
+    assert op.arcsinh(c) == np.arcsinh(c)
+
+def test_arccosh():
+    # Scalar
+    f = op.arccosh(x)
+    assert f._val == np.arccosh(x._val)
+    assert f._der == -np.arccosh(x._val)*np.tanh(x._val)*x_der
+
+    #Constant
+    assert op.arccosh(c) == np.arccosh(c)
+
+def test_arctanh():
+    # Scalar
+    f = op.arctanh(x)
+    assert f._val == np.arctanh(x._val)
+    assert f._der == (1-np.arctanh(x._val)**2)*x_der
+
+    #Constant
+    assert op.arctanh(c) == np.arctanh(c)
+
