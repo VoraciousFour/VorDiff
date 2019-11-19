9a4ee22afd4b8f666a40cb79ca40c97525bf6296
weiru-chen-15801 <54859040+weiru-chen-15801@users.noreply.github.com>
2019-11-19 16:01:32 -0500

    update path

--- VorDiff/tests/test_scalar.py
+++ VorDiff/tests/test_scalar.py
@@ -6,11 +6,11 @@ Created on Fri Nov 15 17:20:36 2019
 @author: weiruchen
 """
 import sys
-sys.path.append('../')
+sys.path.append('../../')
 
 import numpy as np
 
-from nodes.scalar import Scalar
+from VorDiff.nodes.scalar import Scalar
 
 
 # Define scalar object and initialize parameters
