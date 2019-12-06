efd927fce5cd2127a140a83b7cb00a4641dd148d
weiru-chen-15801 <54859040+weiru-chen-15801@users.noreply.github.com>
2019-12-06 18:05:49 -0500

    update test_vector

--- VorDiff/tests/test_vector.py
+++ VorDiff/tests/test_vector.py
@@ -1,8 +1,9 @@
-from VorDiff.nodes.vector import Vector
-#from VorDiff.autodiff import AutoDiff as ad
+#from VorDiff.nodes.vector import Vector
+import vector
+from VorDiff.autodiff import AutoDiff as ad
 import numpy as np
 
-x = Vector([1,2, 3, 4])
+x = ad.vector([1,2, 3, 4])
 a = 6
 b = 2
 
