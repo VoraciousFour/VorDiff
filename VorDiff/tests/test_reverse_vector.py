a6986d80882025ac41eec252bdd089ec896071e4
weiru-chen-15801 <54859040+weiru-chen-15801@users.noreply.github.com>
2019-12-09 15:31:18 -0500

    test for reverse vector

--- VorDiff/tests/test_reverse_vector.py
+++ VorDiff/tests/test_reverse_vector.py
@@ -9,8 +9,8 @@ Created on Sun Dec  8 20:32:48 2019
 import numpy as np
 from VorDiff.nodes.reverse_vector import ReverseVector
 
-
-def generate():
+# create reverse vectors
+def create():
     var1, var2 = ReverseVector([10, 10, 3]), ReverseVector([20, 20, 25])
     var1._init_children()
     var2._init_children()
@@ -21,16 +21,16 @@ def generate():
 c_1, c3, c7 = -1, 3, 7
 
 
-def test_get_item():
+def test_getitem():
     x = ReverseVector([1, 2])
     assert x[0].get() == 1
     assert x[1].get() == 2
 
-test_get_item()
+test_getitem()
 
 
 def test_get():
-    x, y = generate()
+    x, y = create()
     assert (x.get() == x._val).all()
     assert (y.get() == y._val).all()
 
@@ -38,8 +38,8 @@ test_get()
 
 
 
-def test_add():
-    x, y = generate()
+def test_addition():
+    x, y = create()
     f = x + y 
     assert (f.get() == x._val + y._val).all()
     
@@ -50,33 +50,33 @@ def test_add():
     assert (x._gradient == 1).all()
     assert (y._gradient == 1).all()
 
-    x, y = generate()
-    g = y + x
-    assert (g.get() == x._val + y._val).all()
+    x, y = create()
+    f = y + x
+    assert (f.get() == x._val + y._val).all()
 
-    g._gradient = 1
-    g.compute_gradient(x)
-    g.compute_gradient(y)
+    f._gradient = 1
+    f.compute_gradient(x)
+    f.compute_gradient(y)
     assert (x._gradient == 1).all()
     assert (y._gradient == 1).all()
 
-    x, y = generate()
-    h = c7 + x + y + c3
-    assert (h.get() == c7 + x._val + y._val + c3).all()
+    x, y = create()
+    f = c7 + x + y + c3
+    assert (f.get() == c7 + x._val + y._val + c3).all()
 
-    h._gradient = 1
-    h.compute_gradient(x)
-    h.compute_gradient(y)
+    f._gradient = 1
+    f.compute_gradient(x)
+    f.compute_gradient(y)
     assert (x._gradient == 1).all()
     assert (y._gradient == 1).all()
 
-test_add()
+test_addition()
 
 
 
 
-def test_subtract():
-    x, y = generate()
+def test_subtraction():
+    x, y = create()
     f = x - y
     assert (f.get() == x._val - y._val).all()
 
@@ -86,33 +86,33 @@ def test_subtract():
     assert (x._gradient == 1).all()
     assert (y._gradient == -1).all()
 
-    x, y = generate()
-    g = y - x
-    assert (g.get() == y._val - x._val).all()
+    x, y = create()
+    f = y - x
+    assert (f.get() == y._val - x._val).all()
 
-    g._gradient = 1
-    g.compute_gradient(x)
-    g.compute_gradient(y)
+    f._gradient = 1
+    f.compute_gradient(x)
+    f.compute_gradient(y)
     assert (x._gradient == -1).all()
     assert (y._gradient == 1).all()
 
-    x, y = generate()
-    h = c_1 - x - y - c3
-    assert (h.get() == c_1 - x._val - y._val - c3).all()
+    x, y = create()
+    f = c_1 - x - y - c3
+    assert (f.get() == c_1 - x._val - y._val - c3).all()
 
-    h._gradient = 1
-    h.compute_gradient(x)
-    h.compute_gradient(y)
+    f._gradient = 1
+    f.compute_gradient(x)
+    f.compute_gradient(y)
     assert (x._gradient == -1).all()
     assert (y._gradient == -1).all()
 
-test_subtract()
+test_subtraction()
 
 
 
 
-def test_multiply():
-    x, y = generate()
+def test_multiplication():
+    x, y = create()
     f = x * y
     assert (f.get() == x._val * y._val).all()
 
@@ -122,41 +122,41 @@ def test_multiply():
     assert (x._gradient == y._val).all()
     assert (y._gradient == x._val).all()
 
-    x, y = generate()
-    g = y * x
-    assert (g.get() == x._val * y._val).all()
+    x, y = create()
+    f = y * x
+    assert (f.get() == x._val * y._val).all()
 
-    g._gradient = 1
-    g.compute_gradient(x)
-    g.compute_gradient(y)
+    f._gradient = 1
+    f.compute_gradient(x)
+    f.compute_gradient(y)
     assert (x._gradient == y._val).all()
     assert (y._gradient == x._val).all()
 
-    x, y = generate()
-    h = c_1 * x * y * c3
-    assert (h.get() == c_1 * x._val * y._val * c3).all()
+    x, y = create()
+    f = c_1 * x * y * c3
+    assert (f.get() == c_1 * x._val * y._val * c3).all()
 
-    h._gradient = 1
-    h.compute_gradient(x)
-    h.compute_gradient(y)
+    f._gradient = 1
+    f.compute_gradient(x)
+    f.compute_gradient(y)
     assert (x._gradient == c_1 * y._val * c3).all()
     assert (y._gradient == c_1 * x._val * c3).all()
 
-test_multiply()
+test_multiplication()
 
 
 
 
 
 
-def new_generate():
+def new_create():
     vars = ReverseVector([10, 10, 3]), ReverseVector([20, 20, 25]), ReverseVector([30, 30, 1])
     for var in vars:
         var._init_children()
     return tuple(vars)
 
-def test_divide():
-    x, y, z = new_generate()
+def test_division():
+    x, y, z = new_create()
     f = x / y / (1 / z)
     assert (f.get() == x._val / y._val / (1 / z._val)).all()
 
@@ -168,47 +168,47 @@ def test_divide():
     assert (y._gradient == -x._val * z._val / y._val ** 2).all()
     assert (z._gradient == x._val / y._val).all()
 
-    x, y, z = new_generate()
-    g = z / (x * y)
-    assert (g.get() == z._val / (x._val * y._val)).all()
+    x, y, z = new_create()
+    f = z / (x * y)
+    assert (f.get() == z._val / (x._val * y._val)).all()
 
-    g._gradient = 1
-    g.compute_gradient(x)
-    g.compute_gradient(y)
-    g.compute_gradient(z)
+    f._gradient = 1
+    f.compute_gradient(x)
+    f.compute_gradient(y)
+    f.compute_gradient(z)
     assert (x._gradient == -z._val / (x._val ** 2 * y._val)).all()
     assert (y._gradient == -z._val / (x._val * y._val ** 2)).all()
     assert (z._gradient == 1 / (x._val * y._val)).all()
 
-    x, y, z = new_generate()
-    h = c_1 * x / (y * z * c3)
-    assert (h.get() == c_1 * x._val / (y._val * z._val * c3)).all()
+    x, y, z = new_create()
+    f = c_1 * x / (y * z * c3)
+    assert (f.get() == c_1 * x._val / (y._val * z._val * c3)).all()
 
-    h._gradient = 1
-    h.compute_gradient(x)
-    h.compute_gradient(y)
-    h.compute_gradient(z)
+    f._gradient = 1
+    f.compute_gradient(x)
+    f.compute_gradient(y)
+    f.compute_gradient(z)
     assert (x._gradient == c_1 / (y._val * z._val * c3)).all()
     assert (y._gradient == -c_1 * x._val / (y._val * y._val * z._val * c3)).all()
     assert (z._gradient == -c_1 * x._val / (y._val * z._val ** 2 * c3)).all()
 
-    x, y, z = new_generate()
-    w = x / c7
-    assert (w.get() == x._val / c7).all()
+    x, y, z = new_create()
+    f = x / c7
+    assert (f.get() == x._val / c7).all()
 
-    w._gradient = 1
-    w.compute_gradient(x)
+    f._gradient = 1
+    f.compute_gradient(x)
     assert (x._gradient == 1 / c7).all()
 
 
 
-test_divide()
+test_division()
 
 
 
 def test_power():
-    x, y, z = new_generate()
-    x._val, y._val, z._val = np.array([2, 2, 2]), np.array([3, 3, 3]), np.array([4, 4, 4])
+    x, y, z = new_create()
+    x._val, y._val, z._val = np.array([1, 1, 1]), np.array([2, 2, 2]), np.array([3, 3, 3])
     f = x ** y * z
     assert (f.get() == x._val ** y._val * z._val).all()
 
@@ -220,46 +220,46 @@ def test_power():
     assert (y._gradient == x._val ** y._val * z._val * np.log(x._val)).all()
     assert (z._gradient == x._val ** y._val).all()
 
-    x, y, z = new_generate()
-    g = z ** (x * y)
-    assert (g.get() == z._val ** (x._val * y._val)).all()
+#    x, y, z = new_create()
+#    f = z ** (x * y)
+#    assert (f.get() == z._val ** (x._val * y._val)).all()
+#
+#    f._gradient = 1
+#    f.compute_gradient(x)
+#    f.compute_gradient(y)
+#    f.compute_gradient(z)
+#    assert (x._gradient == y._val * np.log(z._val) * z._val ** (x._val * y._val)).all()
+#    assert (y._gradient == x._val * np.log(z._val) * z._val ** (x._val * y._val)).all()
+#    assert (z._gradient == x._val * y._val * z._val ** (x._val * y._val - 1)).all()
 
-    g._gradient = 1
-    g.compute_gradient(x)
-    g.compute_gradient(y)
-    g.compute_gradient(z)
-    assert (x._gradient == y._val * np.log(z._val) * z._val ** (x._val * y._val)).all()
-    assert (y._gradient == x._val * np.log(z._val) * z._val ** (x._val * y._val)).all()
-    assert (z._gradient == x._val * y._val * z._val ** (x._val * y._val - 1)).all()
+    x, y, z = new_create()
+    x._val, y._val, z._val = np.array([1, 1, 1]), np.array([2, 2, 2]), np.array([3, 3, 3])
+    f = c_1 * x ** (y ** z * c3)
+    assert (f.get() == c_1 * x._val ** (y._val ** z._val * c3)).all()
 
-    x, y, z = new_generate()
-    x._val, y._val, z._val = np.array([2, 2, 2]), np.array([3, 3, 3]), np.array([4, 4, 4])
-    h = c_1 * x ** (y ** z * c3)
-    assert (h.get() == c_1 * x._val ** (y._val ** z._val * c3)).all()
-
-    h._gradient = 1
-    h.compute_gradient(x)
-    h.compute_gradient(y)
-    h.compute_gradient(z)
+    f._gradient = 1
+    f.compute_gradient(x)
+    f.compute_gradient(y)
+    f.compute_gradient(z)
     assert (x._gradient == -c3 * y._val ** z._val * x._val ** (c3 * y._val ** z._val - 1)).all()
     assert (y._gradient == (-c3 * z._val * np.log(x._val) * y._val ** (z._val - 1) * x._val ** (c3 * y._val ** z._val))).all()
     assert (z._gradient == (-c3 * np.log(x._val) * y._val ** z._val * np.log(y._val) * x._val ** (c3 * y._val ** z._val))).all()
 
-    x, y, z = new_generate()
-    q = 3 ** x
-    assert (q.get() == 3 ** x._val).all()
+    x, y, z = new_create()
+    f = 3 ** x
+    assert (f.get() == 3 ** x._val).all()
 
-    q._gradient = 1
-    q.compute_gradient(x)
+    f._gradient = 1
+    f.compute_gradient(x)
     assert (x._gradient == 3 ** x._val * np.log(3)).all()
 
     
-    x, y, z = new_generate()
-    w = x ** 2
-    assert (w.get() == x._val ** 2).all()
+    x, y, z = new_create()
+    f = x ** 2
+    assert (f.get() == x._val ** 2).all()
 
-    w._gradient = 1
-    w.compute_gradient(x)
+    f._gradient = 1
+    f.compute_gradient(x)
     assert (x._gradient == 2 * x._val).all()
 
 
