8555171d22cdaddd9bc3472ca6af90f74656d3a3
erik-johnsson <ejohnsson53@gmail.com>
2019-12-10 01:41:53 -0500

    update init

--- VorDiff/__init__.py
+++ VorDiff/__init__.py
@@ -1,1 +1,9 @@
 name = 'VorDiff'
+from VorDiff.autodiff import AutoDiff
+from VorDiff.operator import Operator
+from VorDiff.nodes.scalar import Scalar
+from VorDiff.nodes.vector import Vector, Element
+from VorDiff.reverse_autodiff import ReverseAutoDiff
+from VorDiff.reverse_operator import ReverseOperator
+from VorDiff.nodes.reverse_scalar import ReverseScalar
+from VorDiff.nodes.reverse_vector import ReverseVector
